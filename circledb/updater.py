92d233410a13284a8bd9d003fa8b8c2097047574
miinorii <77248345+miinorii@users.noreply.github.com>
2023-07-15 23:02:07 +0200

    fix memory leak

--- circledb/updater.py
+++ circledb/updater.py
@@ -14,14 +14,15 @@ def update_beatmap_threadpool(token: UserToken, db_path: str, skip_if_in_db=True
 
         with ApiV2(token) as api:
             with concurrent.futures.ThreadPoolExecutor(max_workers=8, thread_name_prefix="pool") as pool:
-                futures = []
+                futures = {}
                 while len(maps_id) > 0:
                     current_batch = [maps_id.pop() for _ in range(50) if len(maps_id) > 0]
-                    futures.append(pool.submit(api.get_beatmaps, ids=current_batch))
+                    futures[pool.submit(api.get_beatmaps, ids=current_batch)] = current_batch
 
                 completed = 0
                 total = len(futures)
                 for future in concurrent.futures.as_completed(futures):
+                    current_batch = futures.pop(future)
                     completed += 1
                     try:
                         data = future.result()
@@ -32,6 +33,7 @@ def update_beatmap_threadpool(token: UserToken, db_path: str, skip_if_in_db=True
                         if completed % 32 == 0 or completed == total:
                             orm.save()
                             logger.info(f"[  \033[1;33m..\033[0m  ] Completion: {completed}/{total}")
+                            gc.collect()
 
 
 def update_lb_multithread(token: UserToken,
