c6407210b58e35c32b1046b9cb7071de50e13da7
miinorii <77248345+miinorii@users.noreply.github.com>
2023-07-16 11:51:16 +0200

    add a one time retry mechanism for failed futures + update country_lb

--- examples/update_db.py
+++ examples/update_db.py
@@ -1,5 +1,11 @@
 from circleapi import UserToken, setup_queue_logging as circleapi_log_queue
-from circledb import update_beatmap_threadpool, setup_queue_logging as circledb_log_queue, update_lb_multithread
+from circledb import (
+    update_beatmap_threadpool,
+    setup_queue_logging as circledb_log_queue,
+    update_lb_multithread,
+    update_country_lb_from_global_lb,
+    update_best_rank_from_lb
+)
 from dotenv import dotenv_values
 import shutil
 
@@ -23,11 +29,26 @@ if __name__ == "__main__":
     )
 
     # Setup empty db
-    # shutil.copy("../ressources/empty_data.db", DB_PATH)
+    shutil.copy("../ressources/empty_data.db", DB_PATH)
 
-    # Update db content
-    # update_beatmap_threadpool(token, DB_PATH, skip_if_in_db=True)
-    update_lb_multithread(token, DB_PATH, "osu", "global", skip_if_in_db=True)
+    # Update db content:
+    # Beatmap
+    update_beatmap_threadpool(token, DB_PATH, skip_if_in_db=True)
+
+    # Global leaderboard
+    failed = update_lb_multithread(token, DB_PATH, "osu", "global", skip_if_in_db=True)
+    if len(failed) > 0:
+        update_lb_multithread(token, DB_PATH, "osu", "global", skip_if_in_db=True)
+
+    # User country lb
+    update_country_lb_from_global_lb(DB_PATH)
+    failed = update_lb_multithread(token, DB_PATH, "osu", "country", skip_if_in_db=True, max_threads=12)
+    if len(failed) > 0:
+        update_lb_multithread(token, DB_PATH, "osu", "country", skip_if_in_db=True, max_threads=12)
+
+    # Country best rank on lb
+    update_best_rank_from_lb(DB_PATH)
+
 
     log_db.stop()
     log_api.stop()
