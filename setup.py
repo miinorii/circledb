2a69fe2f00ac7fcdaa661913ea83c9cacfaa4043
miinorii <77248345+miinorii@users.noreply.github.com>
2023-07-30 22:16:59 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="circledb",
     author="miinorii",
-    version="2023.7.15",
+    version="2023.7.30",
     packages=["circledb"],
     install_requires=["circleapi>=2023.7.15"]
 )
