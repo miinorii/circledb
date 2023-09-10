0e152150b92d7d6e9482fc796d3ee57a15c40d76
miinorii <77248345+miinorii@users.noreply.github.com>
2023-09-10 11:30:37 +0200

    add checkboxes, pagination, filter field, loading messages and automatic column sizing

--- examples/webview/static/script.js
+++ examples/webview/static/script.js
@@ -1,16 +1,50 @@
 async function updateDisplay() {
-    let database = document.getElementById("db-select").value;
-    let table = document.getElementById("table-select").value;
+    // General grid settings
+    const gridOptions = {
+        defaultColDef: {sortable: true, filter: true},
+        rowSelection: "multiple",
+        rowMultiSelectWithClick: true,
+        animateRows: true,
+        enableCellTextSelection: true,
+        ensureDomOrder: true,
+        suppressColumnVirtualisation: true,
+        pagination: true,
+        overlayLoadingTemplate: '<span class="ag-overlay-loading-center">Loading ...</span>'
+    }
 
-    let params = new URLSearchParams({database: database, table: table}).toString();
-    let response = await fetch("/api/read_table_content?" + params);
+    // Create grid
+    const eGridDiv = document.getElementById("table");
+    eGridDiv.innerHTML = "";
+    new agGrid.Grid(eGridDiv, gridOptions);
 
-    let tableData = await response.json()
+    // Fetch data
+    gridOptions.api.showLoadingOverlay();
 
-    let columns = [];
-    tableData.columns.forEach((name) => columns.push({field: name}))
+    const database = document.getElementById("db-select").value;
+    const table = document.getElementById("table-select").value;
 
-    let rows = [];
+    const params = new URLSearchParams({database: database, table: table}).toString();
+    const response = await fetch("/api/read_table_content?" + params);
+
+    const tableData = await response.json()
+
+    // Format data
+    const columns = [];
+    tableData.columns.forEach((name, index) => {
+        // Default column settings
+        let col_params = {
+            field: name, 
+            headerName: name,
+            headerCheckboxSelection: (index === 0) ? true : false,
+            checkboxSelection: (index === 0) ? true : false,
+            headerCheckboxSelectionFilteredOnly: true,
+            floatingFilter: true,
+            resizable: true
+        }
+        columns.push(col_params)
+    })
+
+    const rows = [];
     tableData.rows.forEach((row) => {
         let row_data = {};
         row.forEach((value, index) => {
@@ -19,20 +53,15 @@ async function updateDisplay() {
         rows.push(row_data);
     });
 
-    const gridOptions = {
-        columnDefs: columns,
-        defaultColDef: {sortable: true, filter: true},
-        rowSelection: 'multiple',
-        animateRows: true
-    }
-    const eGridDiv = document.getElementById("table");
-    eGridDiv.innerHTML = "";
-    new agGrid.Grid(eGridDiv, gridOptions);
+    gridOptions.api.hideOverlay();
+    gridOptions.api.setColumnDefs(columns);
     gridOptions.api.setRowData(rows);
+    gridOptions.columnApi.autoSizeAllColumns();
 }
 
 function updateTableList(value){
     let selector = document.getElementById("table-select");
+    selector.value = "";
     selector.disabled = false;
 
     let childrens = selector.children;
